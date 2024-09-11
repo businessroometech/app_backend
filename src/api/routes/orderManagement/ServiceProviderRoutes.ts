73b8c0d118563a9471b873bd29f72568bd055d88
adkood <ashutoshnegi195@gmail.com>
2024-09-11 15:38:43 +0530

    added authenticate middleware

--- src/api/routes/orderManagement/ServiceProviderRoutes.ts
+++ src/api/routes/orderManagement/ServiceProviderRoutes.ts
@@ -13,29 +13,29 @@ import {
 
     getServiceJobsBy_Year_Month_Week,
     totalAmountBy_Year_Month_Week,
-    totalSalesBy_Year_Month_Week,
     getAvgPricePerMonthForCurrentYear,
-    compareAvgPriceWithPreviousMonth
+    compareAvgPriceWithPreviousMonth,
+    totalSalesSubCategoryWise
 } from '@/api/controllers/orderManagement/ServiceProvider';
 import { authenticate } from '@/api/middlewares/auth/Authenticate';
 
 const Router = express.Router();
 
-Router.post('/', getYourServices);
+Router.post('/', authenticate, getYourServices);
 // Router.post('/service-provider/add-service', addService);
-Router.post('/accept', acceptService);
-Router.post('/reject', rejectService);
-Router.post('/complete', completeService);
+Router.post('/accept', authenticate, acceptService);
+Router.post('/reject', authenticate, rejectService);
+Router.post('/complete', authenticate, completeService);
 
 Router.post('/service-management/get', authenticate, getProvidedService);
 Router.post('/service-management/add-or-update', authenticate, addOrUpdateProvidedService);
 Router.delete('/service-management', authenticate, deleteProvidedService);
 
 // home
-Router.post('/by_year_month_week', getServiceJobsBy_Year_Month_Week);
-Router.post('/sales/overview/by_year_month_week', totalAmountBy_Year_Month_Week);
-Router.post('/sales/by_subCategories/by_year_month_week', totalSalesBy_Year_Month_Week);
-Router.post('/avg_order_price_yearly', getAvgPricePerMonthForCurrentYear);
-Router.post('/compare_avg_order_price_monthly', compareAvgPriceWithPreviousMonth);
+Router.post('/by_year_month_week', authenticate, getServiceJobsBy_Year_Month_Week);
+Router.post('/sales/overview/by_year_month_week', authenticate, totalAmountBy_Year_Month_Week);
+Router.post('/sales/by_subCategories/by_year_month_week', authenticate, totalSalesSubCategoryWise);
+Router.post('/avg_order_price_yearly', authenticate, getAvgPricePerMonthForCurrentYear);
+Router.post('/compare_avg_order_price_monthly', authenticate, compareAvgPriceWithPreviousMonth);
 
 export default Router;
--- src/api/routes/profile/ProfileRoutes.ts
+++ src/api/routes/profile/ProfileRoutes.ts
@@ -1,25 +1,23 @@
 import express from 'express';
 
 import { getUser, getPersonalDetails, getEducationalDetails, getFinancialDetails, getProfessionalDetails, setPersonalDetails, setProfessionalDetails, setEducationalDetails, setFinancialDetails, getPersonalDetailsCustomer, setPersonalDetailsCustomer, getBusinessDetails, createOrUpdateBusinessDetails } from '../../controllers/profile/Profile';
+import { authenticate } from '@/api/middlewares/auth/Authenticate';
 
 const Router = express.Router();
 
-Router.post('/me', getUser);
+Router.post('/me', authenticate, getUser);
 
-Router.post('/your-personal-details', getPersonalDetails);
-Router.post('/your-professional-details', getProfessionalDetails);
-Router.post('/your-educational-details/:sectortype', getEducationalDetails);
-Router.post('/your-financial-details', getFinancialDetails);
+Router.post('/your-personal-details', authenticate, getPersonalDetails);
+Router.post('/your-professional-details', authenticate, getProfessionalDetails);
+Router.post('/your-educational-details/:sectortype', authenticate, getEducationalDetails);
+Router.post('/your-financial-details', authenticate, getFinancialDetails);
 Router.post('/your-business-details', getBusinessDetails);
 
-Router.post('/personal', setPersonalDetails);
-Router.post('/professional', setProfessionalDetails);
-Router.post('/educational/:sectortype', setEducationalDetails);
-Router.post('/financial', setFinancialDetails);
-Router.post('/business', createOrUpdateBusinessDetails);
-
-Router.post('/your-personal-details-customer', getPersonalDetailsCustomer);
-Router.post('/personal-customer', setPersonalDetailsCustomer);
+Router.post('/personal', authenticate, setPersonalDetails);
+Router.post('/professional', authenticate, setProfessionalDetails);
+Router.post('/educational/:sectortype', authenticate, setEducationalDetails);
+Router.post('/financial', authenticate, setFinancialDetails);
+Router.post('/business', authenticate, createOrUpdateBusinessDetails);
 
 Router.post('/your-personal-details-customer', getPersonalDetailsCustomer);
 Router.post('/personal-customer', setPersonalDetailsCustomer);
