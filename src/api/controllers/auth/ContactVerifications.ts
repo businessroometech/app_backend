4c914a564889b190d882a8bf2f34c44837ea38e3
raj-kartik <rajkartik159@gmail.com>
2024-10-08 14:18:15 +0530

    login otp is given

--- src/api/controllers/auth/ContactVerifications.ts
+++ src/api/controllers/auth/ContactVerifications.ts
@@ -5,6 +5,7 @@ import { parsePhoneNumberFromString } from 'libphonenumber-js';
 import { UserLogin } from '../../entity/user/UserLogin';
 import { OtpVerification } from '@/api/entity/others/OtpVerification';
 import { AppDataSource } from '@/server';
+import NotificationController from '../notifications/Notification';
 
 const generateVerificationCode = (): string => {
   return Math.floor(1000 + Math.random() * 9000).toString();
@@ -21,7 +22,6 @@ export const sendVerificationCode = async (req: Request, res: Response): Promise
     const otpVerificationRepository = AppDataSource.getRepository(OtpVerification);
 
     if (useCase === 'Signup') {
-
       if (!mobileNumber) {
         return res.status(400).json({ status: 'error', message: 'Please provide mobile number for this use case.' });
       }
@@ -35,9 +35,7 @@ export const sendVerificationCode = async (req: Request, res: Response): Promise
           data: { user: existingUser },
         });
       }
-    }
-    else if (useCase === 'Forgot Password') {
-
+    } else if (useCase === 'Forgot Password') {
       if (!mobileNumber) {
         return res.status(400).json({ status: 'error', message: 'Please provide mobile number for this use case.' });
       }
@@ -50,8 +48,7 @@ export const sendVerificationCode = async (req: Request, res: Response): Promise
           message: 'This mobile number is not registered',
         });
       }
-    }
-    else {
+    } else {
       // For other use cases, require userId and fetch the user
       if (!userId) {
         return res.status(400).json({ status: 'error', message: 'Please provide userId for this use case.' });
@@ -87,13 +84,15 @@ export const sendVerificationCode = async (req: Request, res: Response): Promise
     }
 
     const currentDate = new Date();
-    const cooldownPeriod = 1 * 60 * 1000;  // 1 minute cooldown
-    const expirationPeriod = 5 * 60 * 1000;  // 5 minutes expiration
+    const cooldownPeriod = 1 * 60 * 1000; // 1 minute cooldown
+    const expirationPeriod = 5 * 60 * 1000; // 5 minutes expiration
 
     if (otpObj) {
       const lastSentAt = new Date(otpObj.updatedAt);
       if (currentDate.getTime() - lastSentAt.getTime() < cooldownPeriod) {
-        return res.status(429).json({ status: 'error', message: 'Please wait before requesting a new verification code.' });
+        return res
+          .status(429)
+          .json({ status: 'error', message: 'Please wait before requesting a new verification code.' });
       }
 
       otpObj.verificationCode = code;
@@ -101,22 +100,47 @@ export const sendVerificationCode = async (req: Request, res: Response): Promise
       otpObj.expiresAt = new Date(currentDate.getTime() + expirationPeriod);
       await otpObj.save();
     } else {
-      otpObj = await otpVerificationRepository.create({
-        userId: (useCase === 'Signup' || useCase === 'Forgot Password') ? null : userId,
-        mobileNumber,
-        verificationCode: code,
-        isVerified: false,
-        expiresAt: new Date(currentDate.getTime() + expirationPeriod),
-        userType,
-        sentTo: 'Mobile',
-        useCase,
-      }).save();
+      otpObj = await otpVerificationRepository
+        .create({
+          userId: useCase === 'Signup' || useCase === 'Forgot Password' ? null : userId,
+          mobileNumber,
+          verificationCode: code,
+          isVerified: false,
+          expiresAt: new Date(currentDate.getTime() + expirationPeriod),
+          userType,
+          sentTo: 'Mobile',
+          useCase,
+        })
+        .save();
+    }
+
+    try {
+      // Make sure this function doesn't send its own response
+      await NotificationController.sendNotification(
+        {
+          body: {
+            notificationType: 'sms',
+            templateName: 'login_otp',
+            recipientId: user?.id,
+            recipientType: user?.primaryRole === 'Customer' ? 'Customer' : 'ServiceProvider',
+            data: {
+              OTP: code,
+              'Company Name': 'Connect',
+            },
+          },
+        } as Request,
+        res
+      );
+    } catch (err: any) {
+      console.error('Signup successful but error sending SMS:', err.message || err);
     }
 
     return res.status(200).json({
-      status: 'success', message: 'Verification code sent successfully.', data: {
-        code
-      }
+      status: 'success',
+      message: 'Verification code sent successfully.',
+      data: {
+        code,
+      },
     });
   } catch (err) {
     console.error(err);
@@ -124,15 +148,21 @@ export const sendVerificationCode = async (req: Request, res: Response): Promise
   }
 };
 
-
 export const verifyCode = async (req: Request, res: Response): Promise<Response> => {
   try {
     const { userId, verificationCode, useCase, mobileNumber } = req.body;
 
-    if ((!userId && (useCase !== 'Signup' && useCase !== 'Forgot Password')) || !verificationCode || !useCase || ((useCase === 'Signup' || useCase === 'Forgot Password') && !mobileNumber)) {
-      return res
-        .status(400)
-        .json({ status: 'error', message: 'Please provide userId (for start-work and finish-work), mobileNumber (for Signup and forgot-password), verification code, and useCase.' });
+    if (
+      (!userId && useCase !== 'Signup' && useCase !== 'Forgot Password') ||
+      !verificationCode ||
+      !useCase ||
+      ((useCase === 'Signup' || useCase === 'Forgot Password') && !mobileNumber)
+    ) {
+      return res.status(400).json({
+        status: 'error',
+        message:
+          'Please provide userId (for start-work and finish-work), mobileNumber (for Signup and forgot-password), verification code, and useCase.',
+      });
     }
 
     const otpVerificationRepository = AppDataSource.getRepository(OtpVerification);
@@ -165,7 +195,6 @@ export const verifyCode = async (req: Request, res: Response): Promise<Response>
   }
 };
 
-
 // ---------------------FOR MOBILE APP-------------------------------
 
 export const sendVerificationCode_mobile_app = async (req: Request, res: Response): Promise<void> => {
@@ -173,7 +202,7 @@ export const sendVerificationCode_mobile_app = async (req: Request, res: Respons
     const { mobileNumber, createdBy, updatedBy } = req.body;
 
     if (!mobileNumber) {
-      res.status(400).json({ status: "error", message: "Please provide Mobile number" });
+      res.status(400).json({ status: 'error', message: 'Please provide Mobile number' });
       return;
     }
 
@@ -184,18 +213,21 @@ export const sendVerificationCode_mobile_app = async (req: Request, res: Respons
     let user = await userLoginRepository.findOne({ where: { mobileNumber } });
 
     if (!user) {
-      user = await userLoginRepository.create({
-        mobileNumber,
-        password: "",
-        primaryRole: 'Customer',
-        userType: 'Individual',
-        createdBy: createdBy || 'system',
-        updatedBy: updatedBy || 'system',
-      }).save();
-    }
-    else {
+      user = await userLoginRepository
+        .create({
+          mobileNumber,
+          password: '',
+          primaryRole: 'Customer',
+          userType: 'Individual',
+          createdBy: createdBy || 'system',
+          updatedBy: updatedBy || 'system',
+        })
+        .save();
+    } else {
       if (user?.primaryRole === 'ServiceProvider') {
-        res.status(400).json({ status: "error", message: "This mobile number is already registered as Service Provider" });
+        res
+          .status(400)
+          .json({ status: 'error', message: 'This mobile number is already registered as Service Provider' });
         return;
       }
     }
@@ -211,11 +243,11 @@ export const sendVerificationCode_mobile_app = async (req: Request, res: Respons
     const code = generateVerificationCode();
     console.log('generated code: ', code);
 
-    let otpObj = await otpVerificationRepository.findOne({ where: { mobileNumber, useCase: "Signup" } });
+    let otpObj = await otpVerificationRepository.findOne({ where: { mobileNumber, useCase: 'Signup' } });
 
     const currentDate = new Date();
-    const cooldownPeriod = 1 * 60 * 1000;  // 1 minute cooldown
-    const expirationPeriod = 5 * 60 * 1000;  // 5 minutes expiration
+    const cooldownPeriod = 1 * 60 * 1000; // 1 minute cooldown
+    const expirationPeriod = 5 * 60 * 1000; // 5 minutes expiration
 
     if (otpObj) {
       const lastSentAt = new Date(otpObj.updatedAt);
@@ -227,17 +259,42 @@ export const sendVerificationCode_mobile_app = async (req: Request, res: Respons
       otpObj.verificationCode = code;
       otpObj.isVerified = false;
       otpObj.expiresAt = new Date(currentDate.getTime() + expirationPeriod);
+
       await otpObj.save();
     } else {
-      otpObj = await otpVerificationRepository.create({
-        mobileNumber,
-        verificationCode: code,
-        isVerified: false,
-        expiresAt: new Date(currentDate.getTime() + expirationPeriod),
-        userType: 'Customer',
-        sentTo: 'Mobile',
-        useCase: 'Signup'
-      }).save();
+      otpObj = await otpVerificationRepository
+        .create({
+          mobileNumber,
+          verificationCode: code,
+          isVerified: false,
+          expiresAt: new Date(currentDate.getTime() + expirationPeriod),
+          userType: 'Customer',
+          sentTo: 'Mobile',
+          useCase: 'Signup',
+        })
+        .save();
+    }
+
+
+    try {
+      // Make sure this function doesn't send its own response
+      await NotificationController.sendNotification(
+        {
+          body: {
+            notificationType: 'sms',
+            templateName: 'login_otp',
+            recipientId: user?.id,
+            recipientType: user?.primaryRole === 'Customer' ? 'Customer' : 'ServiceProvider',
+            data: {
+              OTP: code,
+              'Company Name': 'Connect',
+            },
+          },
+        } as Request,
+        res
+      );
+    } catch (err: any) {
+      console.error('Signup successful but error sending SMS:', err.message || err);
     }
 
     //-------SMS sending logic---------------
@@ -247,4 +304,4 @@ export const sendVerificationCode_mobile_app = async (req: Request, res: Respons
     res.status(500).json({ status: 'error', message: 'Server error.' });
     return;
   }
-}
+};
