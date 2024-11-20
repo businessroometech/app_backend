9289804e0be2a827afd3bb9d08d9d18f95ca3870
raj-kartik <rajkartik159@gmail.com>
2024-11-20 12:05:27 +0530

    i have made the booking event controller

--- src/api/controllers/event/TicketEventController.ts
+++ src/api/controllers/event/TicketEventController.ts
@@ -1,25 +1,30 @@
 import { Event } from '@/api/entity/eventManagement/Event';
+import { EventBooking } from '@/api/entity/eventManagement/EventBooking';
 import { Ticket } from '@/api/entity/eventManagement/Ticket';
 import { Sector } from '@/api/entity/sector/Sector';
 import { UserLogin } from '@/api/entity/user/UserLogin';
+import { validateRequestBody } from '@/common/utils/requestBodyValidation';
 import { validateAndFetchEntities, ValidationConfig } from '@/components/validateFields';
 import { AppDataSource } from '@/server';
 import { Request, Response } from 'express';
+import { object } from 'zod';
 
 export const getCreatedAllTicket = async (req: Request, res: Response) => {
-  const userLoginRepository = AppDataSource.getRepository(UserLogin);
-  const eventRepository = AppDataSource.getRepository(Event);
   const ticketRepository = AppDataSource.getRepository(Ticket);
-  const validationConfigs: ValidationConfig[] = [
-    { field: 'userId', repository: userLoginRepository, errorMessage: 'Please provide userId' },
-    { field: 'eventId', repository: eventRepository, errorMessage: 'Please provide sectorId' },
-  ];
+  const validationRules = {
+    // eventId: { required: true, type: 'string' },
+    userId: { required: true, type: 'string' },
+  };
+  const errors = validateRequestBody(req.body, validationRules);
+  if (errors) {
+    return res.status(400).json({ errors });
+  }
 
-  const entities = await validateAndFetchEntities(req, res, validationConfigs);
-  if (!entities) return;
+  const { userId } = req.body;
+  if (!userId) return res.status(400).json({ status: 'success', message: 'User is not found' });
 
   try {
-    const ticketDetails = await ticketRepository.find();
+    const ticketDetails = await ticketRepository.find({ where: userId });
     return res
       .status(200)
       .json({ status: 'success', message: 'Created Ticket Event successfully', data: ticketDetails });
@@ -30,15 +35,15 @@ export const getCreatedAllTicket = async (req: Request, res: Response) => {
 };
 
 export const getCreatedTicketDetails = async (req: Request, res: Response) => {
-  const userLoginRepository = AppDataSource.getRepository(UserLogin);
-  const eventRepository = AppDataSource.getRepository(Event);
   const ticketRepository = AppDataSource.getRepository(Ticket);
-  const validationConfigs: ValidationConfig[] = [
-    { field: 'userId', repository: userLoginRepository, errorMessage: 'Please provide userId' },
-    { field: 'eventId', repository: eventRepository, errorMessage: 'Please provide sectorId' },
-    { field: 'id', repository: ticketRepository, errorMessage: 'Please provide ticketId' },
-  ];
-
+  const validationRules = {
+    eventId: { required: true, type: 'string' },
+    userId: { required: true, type: 'string' },
+  };
+  const errors = validateRequestBody(req.body, validationRules);
+  if (errors) {
+    return res.status(400).json({ errors });
+  }
   const entities = await validateAndFetchEntities(req, res, validationConfigs);
   if (!entities) return;
 
@@ -53,19 +58,26 @@ export const getCreatedTicketDetails = async (req: Request, res: Response) => {
 };
 
 export const getBookedAllTicket = async (req: Request, res: Response) => {
-  const userLoginRepository = AppDataSource.getRepository(UserLogin);
-  const eventRepository = AppDataSource.getRepository(Event);
   const ticketRepository = AppDataSource.getRepository(Ticket);
-  const validationConfigs: ValidationConfig[] = [
-    { field: 'userId', repository: userLoginRepository, errorMessage: 'Please provide userId' },
-    { field: 'eventId', repository: eventRepository, errorMessage: 'Please provide sectorId' },
-  ];
+  const bookedEventRepository = AppDataSource.getRepository(EventBooking);
 
-  const entities = await validateAndFetchEntities(req, res, validationConfigs);
-  if (!entities) return;
+  const validationRules = {
+    // eventId: { required: true, type: 'string' },
+    userId: { required: true, type: 'string' },
+  };
+  const errors = validateRequestBody(req.body, validationRules);
+  if (errors) {
+    return res.status(400).json({ errors });
+  }
+
+  const { userId, eventId } = req.body;
+  if (!userId) return res.status(400).json({ status: 'success', message: 'User is not found' });
 
   try {
-    const ticketDetails = await ticketRepository.find();
+    const bookedEvents = await bookedEventRepository.find({ where: eventId });
+    console.log('----booked events data id ----', bookedEvents);
+
+    const ticketDetails = await ticketRepository.find({ where: userId });
     return res
       .status(200)
       .json({ status: 'success', message: 'Created Ticket fetched successfully', data: ticketDetails });
@@ -76,22 +88,71 @@ export const getBookedAllTicket = async (req: Request, res: Response) => {
 };
 
 export const getBookedTicketDetails = async (req: Request, res: Response) => {
-  const userLoginRepository = AppDataSource.getRepository(UserLogin);
-  const eventRepository = AppDataSource.getRepository(Event);
   const ticketRepository = AppDataSource.getRepository(Ticket);
-  const validationConfigs: ValidationConfig[] = [
-    { field: 'userId', repository: userLoginRepository, errorMessage: 'Please provide userId' },
-    { field: 'eventId', repository: eventRepository, errorMessage: 'Please provide sectorId' },
-    { field: 'id', repository: ticketRepository, errorMessage: 'Please provide ticketId' },
-  ];
 
-  const entities = await validateAndFetchEntities(req, res, validationConfigs);
-  if (!entities) return;
+  const validationRules = {
+    eventId: { required: true, type: 'string' },
+  };
+  const errors = validateRequestBody(req.body, validationRules);
+  if (errors) {
+    return res.status(400).json({ errors });
+  }
 
-  const { id } = req.body;
+  const { eventId } = req.body;
+  if (!eventId) return res.status(400).json({ status: 'bad request', message: 'please provdide event details' });
+
   try {
-    const ticketDetails = await ticketRepository.find({ where: { id } });
+    const ticketDetails = await ticketRepository.find({ where: { eventId } });
     return res.status(200).json({ status: 'success', message: 'Ticket successfully', data: ticketDetails });
+  } catch (error) {
+    console.error('Error creating invoice:', error);
+    return res.status(500).json({ status: 'error', message: 'Error creating invoice' });
+  }
+};
+
+export const bookingTicket = async (req: Request, res: Response) => {
+  const bookingRepository = AppDataSource.getRepository(EventBooking);
+  // const eventRepository = AppDataSource.getRepository(Event);
+  const ticketRepository = AppDataSource.getRepository(Ticket);
+  const validationRules = {
+    // eventId: { required: true, type: 'string' },
+    userId: { required: true, type: 'string' },
+    eventId: { required: true, type: 'string' },
+    ticketId: { required: true, type: 'string' },
+  };
+
+  const errors = validateRequestBody(req.body, validationRules);
+  if (errors) {
+    return res.status(400).json({ errors });
+  }
+
+  const { userId, eventId, ticketId, orderId, transactionId, price } = req.body;
+
+  try {
+    if (!price) {
+      let bookingUser = bookingRepository.create({
+        userId,
+        eventId,
+        ticketId,
+        amountPaid:price,
+        bookingDate:new Date()
+      });
+
+      const bookingConfirm = await bookingUser.save();
+      // const bookingId = bookingConfirm?.id;
+
+      let ticket = await ticketRepository.find({ where: { id: ticketId } });
+
+      Object.assign(ticket, {
+        id: ticketId,
+        eventId: eventId,
+        price: price,
+        quantityAvailable: ticket.quantityAvailable - 1,
+      });
+      return res
+        .status(200)
+        .json({ status: 'success', message: 'Created Ticket Event successfully', data: bookingConfirm });
+    }
   } catch (error) {
     console.error('Error creating invoice:', error);
     return res.status(500).json({ status: 'error', message: 'Error creating invoice' });
