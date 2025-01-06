6ae4e099924b6eb958664ecc3d20158284cb17fb
adkood <ashutoshnegi195@gmail.com>
2025-01-06 20:28:14 +0530

    s3 api created

--- src/api/routes/auth/AuthRoutes.ts
+++ src/api/routes/auth/AuthRoutes.ts
@@ -4,11 +4,15 @@ import { login } from '../../controllers/auth/Login';
 import { logout } from '../../controllers/auth/Logout';
 import { signup } from '../../controllers/auth/Signup';
 import { authenticate } from '../../middlewares/auth/Authenticate';
+import { generateUploadUrl, getDocumentFromBucket } from "../../controllers/s3/awsControllers";
 
 const Router = express.Router();
 
 Router.post('/signup', signup);
 Router.post('/login', login);
 Router.post('/logout', authenticate, logout);
+
+Router.post('/generate-upload-url', generateUploadUrl);
+Router.post('/document-retrival', getDocumentFromBucket);
 
 export default Router;
