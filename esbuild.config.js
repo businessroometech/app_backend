097fd02a7aa9e1dc7e68532d7aece47a19632d8b
adkood <ashutoshnegi195@gmail.com>
2025-01-15 20:32:52 +0530

    build file changed

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,19 +1,28 @@
 const { build } = require('esbuild');
 const { dependencies } = require('./package.json');
 
+// Get all dependencies to mark as external
 const externalDependencies = Object.keys(dependencies || {});
 
 build({
-  entryPoints: ["src/**/*.ts"],  // Main entry point, imports will be followed
-  bundle: true,
-  platform: 'node', 
-  external: ['typeorm', 'reflect-metadata'], // Mark TypeORM as external// Avoid bundling external dependencies
+  entryPoints: ['./src/index.ts'],  // Set the correct main entry file
+  bundle: true,                    // Bundle the app into a single file
+  platform: 'node',                // Specify Node.js platform
+  target: 'es2020',                // JavaScript version to compile to
+  format: 'cjs',                   // Output format (CommonJS)
   outdir: 'dist',                  // Output directory
-  sourcemap: true,                // Set to true for production
-  target: ['es2020'],               // JavaScript target
-  tsconfig: './tsconfig.json',      // TypeScript config
-  format: 'cjs',
- // external: ['src/swagger_output.json','swagger-ui-express']                    // CommonJS format
+  sourcemap: true,                 // Generate sourcemaps for debugging
+  tsconfig: './tsconfig.json',     // Use your TypeScript config file
+  external: [
+    ...externalDependencies,       // Exclude all dependencies from the bundle
+    'typeorm', 
+    'reflect-metadata',
+    'swagger-ui-express',          // Additional externals
+    './src/swagger_output.json'    // Avoid bundling JSON files
+  ],
 }).then(() => {
-  console.log('Build finished');
-}).catch(() => process.exit(1));
+  console.log('Build finished successfully');
+}).catch((error) => {
+  console.error('Build failed:', error);
+  process.exit(1);
+});
