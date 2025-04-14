1be387583b87e61ad50c38a5364eb57401f87c76
girrajkushwahin <tgirrajindia@gmail.com>
2025-04-14 19:09:43 +0530

    edited controller/auth/DeleteUserPhoto.ts file

--- src/api/controllers/auth/DeleteUserPhoto.ts
+++ src/api/controllers/auth/DeleteUserPhoto.ts
@@ -7,32 +7,40 @@ interface AuthenticatedRequest extends Request {
 }
 
 export const DeleteUserPhoto = async (req: AuthenticatedRequest, res: Response): Promise<Response> => {
-  const userId = req.userId;
-  const { targetUserId, isProfile } = req.body;
+  try {
+    const userId = req.userId;
+    const { targetUserId, isProfile } = req.body;
 
-  if (!userId) return res.status(400).json({ success: false, message: 'Invalid userId' });
+    if (!userId) return res.status(400).json({ success: false, message: 'Invalid userId' });
 
-  const personalRepo = AppDataSource.getRepository(PersonalDetails);
-  const isAdmin = await personalRepo.findOne({
-    where: { isAdmin: true },
-  });
+    const personalRepo = AppDataSource.getRepository(PersonalDetails);
+    const isAdmin = await personalRepo.findOne({
+      where: { isAdmin: true },
+    });
 
-  if (!isAdmin)
-    return res.status(403).json({ success: false, message: 'You are not authenticated to perform this action' });
+    if (!isAdmin)
+      return res.status(403).json({ success: false, message: 'You are not authenticated to perform this action' });
 
-  const user = await personalRepo.findOne({
-    where: { id: targetUserId },
-  });
+    const user = await personalRepo.findOne({
+      where: { id: targetUserId },
+    });
 
-  if (!user) return res.status(404).json({ success: false, message: 'Invalid userId' });
+    if (!user) return res.status(404).json({ success: false, message: 'Invalid userId' });
 
-  if (isProfile) {
-    user.profilePictureUploadId = '';
-  } else {
-    user.bgPictureUploadId = '';
+    if (isProfile) {
+      user.profilePictureUploadId = '';
+    } else {
+      user.bgPictureUploadId = '';
+    }
+
+    await personalRepo.save(user);
+
+    return res.status(200).json({ success: true, message: 'User photo deleted by admin' });
+  } catch (err: any) {
+    console.error('ERROR => ', err);
+    return res.status(500).json({
+      message: 'Internal server error',
+      error: err.message,
+    });
   }
-
-  await personalRepo.save(user);
-
-  return res.status(200).json({ success: true, message: 'User photo deleted by admin' });
 };
