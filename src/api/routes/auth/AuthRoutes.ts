581f1535c14bc270e66560b43a47df2ae65ed91a
girrajkushwahin <tgirrajindia@gmail.com>
2025-04-14 17:36:48 +0530

    feat: delete user profile photo & bg photo by admin

--- src/api/routes/auth/AuthRoutes.ts
+++ src/api/routes/auth/AuthRoutes.ts
@@ -4,15 +4,23 @@ import { disable, farmaan, login } from '../../controllers/auth/Login';
 import { logout } from '../../controllers/auth/Logout';
 import { signup } from '../../controllers/auth/Signup';
 import { authenticate } from '../../middlewares/auth/Authenticate';
-import { generateUploadUrl, getDocumentFromBucket } from "../../controllers/s3/awsControllers";
-import { findUserByUserName, getUserProfile, ProfileVisitController, searchUserProfile, UpdateUserProfile, uploadProfileAndCover } from '@/api/controllers/profile/UserProfile';
-import { sendResetEmail, resetPassword } from "@/api/controllers/auth/ResetPassword";
+import { generateUploadUrl, getDocumentFromBucket } from '../../controllers/s3/awsControllers';
+import {
+  findUserByUserName,
+  getUserProfile,
+  ProfileVisitController,
+  searchUserProfile,
+  UpdateUserProfile,
+  uploadProfileAndCover,
+} from '@/api/controllers/profile/UserProfile';
+import { sendResetEmail, resetPassword } from '@/api/controllers/auth/ResetPassword';
 import { getOnlineUsers } from '@/api/controllers/chat/Message';
 import { sendVerificationEmail, verifyEmail } from '@/api/controllers/auth/EmailVerification';
 import { getInTouch, createAccount, transfer } from '@/api/controllers/LandingPage/LandingPage';
 // import { uploadMiddleware2 } from '@/api/controllers/posts/UserPost';
 // import { getOnlineUsers } from "../../../socket";
 // import { sendVerificationEmail, verifyEmail } from '@/api/controllers/auth/EmailVerification';
+import { DeleteUserPhoto } from '../../controllers/auth/DeleteUserPhoto';
 
 const Router = express.Router();
 
@@ -28,13 +36,12 @@ Router.post('/verify-email', verifyEmail);
 Router.post('/generate-upload-url', generateUploadUrl);
 Router.post('/document-retrival', getDocumentFromBucket);
 
-Router.post('/update-or-create-Profile', authenticate, uploadProfileAndCover, UpdateUserProfile)
-Router.get('/get-user-Profile', authenticate, getUserProfile)
+Router.post('/update-or-create-Profile', authenticate, uploadProfileAndCover, UpdateUserProfile);
+Router.get('/get-user-Profile', authenticate, getUserProfile);
 
-
-Router.post('/recored-visit', authenticate, ProfileVisitController.recordVisit)
-Router.get('/get-profile-visit', authenticate, ProfileVisitController.getMyProfileVisits)
-Router.get('/get-profile-visited', authenticate, ProfileVisitController.getProfilesIVisited)
+Router.post('/recored-visit', authenticate, ProfileVisitController.recordVisit);
+Router.get('/get-profile-visit', authenticate, ProfileVisitController.getMyProfileVisits);
+Router.get('/get-profile-visited', authenticate, ProfileVisitController.getProfilesIVisited);
 
 Router.post('/online-users', getOnlineUsers);
 Router.get('/users', authenticate, searchUserProfile);
@@ -48,5 +55,6 @@ Router.post('/disable-me', authenticate, disable);
 
 Router.post('/farmaan-bhejdo', farmaan);
 
-export default Router;
+Router.post('/delete-user-photo', authenticate, DeleteUserPhoto);
 
+export default Router;
