439ca91e6e73e3d2f8db4bd79f2062fbf19e7892
girrajkushwahin <tgirrajindia@gmail.com>
2025-04-16 04:00:00 +0530

    bug fix: disposable email feature fixed

--- src/api/middlewares/disposable-mail/disposable.ts
+++ src/api/middlewares/disposable-mail/disposable.ts
@@ -1,19 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const disposableDomains = new Set(
-  fs
-    .readFileSync(path.join(__dirname, 'mailList.txt'), 'utf8')
-    .split('\n')
-    .map((domain) => domain.trim().toLowerCase())
-    .filter(Boolean)
-);
+import { disposableMails } from './data';
 
 export function isDisposable(email: any) {
   const domain = email.split('@')[1].toLowerCase().trim();
-  return disposableDomains.has(domain);
+  return disposableMails.includes(domain);
 }
