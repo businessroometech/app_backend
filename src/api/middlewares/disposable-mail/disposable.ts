863f0215fcc49a3f219e8160b72941106efef4bd
adkood <ashutoshnegi195@gmail.com>
2025-04-16 03:07:16 +0530

    chnages in disposable.ts

--- src/api/middlewares/disposable-mail/disposable.ts
+++ src/api/middlewares/disposable-mail/disposable.ts
@@ -1,9 +1,25 @@
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const disposableDomains = new Set(
+//   fs
+//     .readFileSync(path.join(__dirname, 'mailList.txt'), 'utf8')
+//     .split('\n')
+//     .map((domain) => domain.trim().toLowerCase())
+//     .filter(Boolean)
+// );
+
+// export function isDisposable(email: any) {
+//   const domain = email.split('@')[1].toLowerCase().trim();
+//   return disposableDomains.has(domain);
+// }
+
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const disposableDomains = new Set(
   fs
@@ -13,7 +29,7 @@ const disposableDomains = new Set(
     .filter(Boolean)
 );
 
-export function isDisposable(email: any) {
+export function isDisposable(email: string) {
   const domain = email.split('@')[1].toLowerCase().trim();
   return disposableDomains.has(domain);
 }
